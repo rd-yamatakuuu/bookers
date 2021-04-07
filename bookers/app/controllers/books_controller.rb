ca3bc553fe9d6d609282d29e8c206340a3247b38
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-04-07 16:40:57 +0000

    [Fix]before-_test

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -5,12 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice]='Book was successfully created.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render action: :index
+      @books = Book.all
+      render :index
     end
   end
 
@@ -27,12 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice]='Book was successfully updated'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render action: :edit
+      @books = Book.all
+      render :edit
     end
   end
 
@@ -42,7 +44,8 @@ class BooksController < ApplicationController
       flash[:notice]='Book was successfully destroyed'
       redirect_to books_path
     else
-      render action: :index
+      @books = Book.all
+      render :index
     end
   end
 
